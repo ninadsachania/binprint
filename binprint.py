c8d56832dfedb131a254e0c070f259471acbc063
Ninad Sachania <ninad.sachania@gmail.com>
2019-06-03 19:47:08 +0530

    Add dec_to_hex() function

--- binprint.py
+++ binprint.py
@@ -33,5 +33,33 @@ def dec_to_bin(number: int) -> str:
     return '0b' + ''.join(str(x) for x in digits)
 
 
+def dec_to_hex(number: int) -> str:
+    number = int(number)
+
+    # List where individual binary digits will be stored
+    digits = []
+
+    while number > 0:
+        # The quotient
+        quotient = number // 16
+        remainder = number % 16
+
+        # Trick to convert 10, 11, 12, 13, 14, 15
+        # into A, B, C, D, E, F respectively
+        if remainder >= 10:
+            # Getting the remainder of the remainder gives an offset
+            # which is then added to the base value 65
+            remainder = chr(65 + (remainder % 10))
+
+        digits.append(remainder)
+        number = quotient
+
+    digits.reverse()
+
+    # Return the hexadecimal number a string
+    return '0x' + ''.join(str(x) for x in digits)
+
+
 for number in NUMBERS:
     print(dec_to_bin(number))
+    print(dec_to_hex(number))
